b2a4a8f638d589a4be95f8e752b020868294592f
Wang-weixi <wangweixiandrew@gmail.com>
2021-09-29 18:22:04 +0700

    weixi

--- deblur/test.py
+++ deblur/test.py
@@ -27,8 +27,6 @@ net = get_net(input_depth,'multi_skip', pad,
                 upsample_mode='bilinear').type(dtype)
 net.load_state_dict(torch.load('net_down_final.pth'))
 net = torch.nn.DataParallel(net)
-
-
 net2=DLSS.DLSS(1)
 net2.load_state_dict(torch.load('net_up.pth'))
 net2=net2.type(dtype)
@@ -39,23 +37,18 @@ Img_list=sorted(os.listdir(opt.input_path))
 for img_name in Img_list:
     file_name=opt.input_path+img_name
     img = np.array(cv2.imread(file_name, -1), dtype=np.float32)/255.
-    #img=cv2.resize(img, dsize=(1180, 730), interpolation=cv2.INTER_CUBIC)
+    H,W=img.shape
+    img=cv2.resize(img, dsize=(int(W/2), int(H/2)), interpolation=cv2.INTER_CUBIC)
     img=1-img/255.
     if img.ndim == 2:
         Img = np.expand_dims(img, axis=0)
-
     else:
         Img = np.transpose(img,(2,0,1))
- 
-    c,w,h = Img.shape
-
     Img = np.expand_dims(Img,axis=0)
-
     Img_tensor =  torch.FloatTensor(Img).cuda()
     with torch.no_grad():
-        out=1-Img_tensor.data
-    path=opt.output_path+img_name[:-4]+'.png'
+        out=1-net2(net(Img_tensor)).data
+    path=opt.output_path+img_name[0:-4]+'.png'
     out_img=torch.clamp(out.squeeze().cpu(),0.,1.)
     out_img=out_img.detach().numpy()
-    print(out_img.shape)
     cv2.imwrite(path,255*out_img)
